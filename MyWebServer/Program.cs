d2638669c32dde08d118d6919dbcee61c8a06f33
An Truong <truongtaman2004@gmail.com>
2026-03-17 21:17:40 +0700

    Add project files.

---
+++ MyWebServer/Program.cs
@@ -0,0 +1,14 @@
+namespace MyWebServer
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = Host.CreateApplicationBuilder(args);
+			builder.Services.AddHostedService<Worker>();
+
+			var host = builder.Build();
+			host.Run();
+		}
+	}
+}
